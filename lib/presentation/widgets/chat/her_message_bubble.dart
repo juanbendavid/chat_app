237f5477765100f38249168cf5e797426c2e7295
juanbendavid <56058007+juanbendavid@users.noreply.github.com>
2023-10-31 22:55:24 -0300

    loading

--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -39,6 +39,15 @@ class _ImageBubble extends StatelessWidget {
         width: size.width * 0.7,
         height: 150,
         fit: BoxFit.cover,
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) return child;
+          return Container(
+            width: size.width * 0.7,
+            height: 150,
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            child: const Text("Se está cargando  una imagen"),
+          );
+        },
       ),
     );
   }
